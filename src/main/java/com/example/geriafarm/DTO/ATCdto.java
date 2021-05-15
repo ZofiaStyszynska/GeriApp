6ef0bf0f92572d25c4141b97f847cd86bd670d5c
ZofiaStyszynska <ZofiaStyszynska@users.noreply.github.com>
2021-05-15 21:48:16 +0200

    branch - ATC as entity with more specific fields

--- src/main/java/com/example/geriafarm/DTO/ATCdto.java
+++ src/main/java/com/example/geriafarm/DTO/ATCdto.java
@@ -1,14 +1,28 @@
 package com.example.geriafarm.DTO;
 
 import com.example.geriafarm.entities.ATC;
+import lombok.Builder;
 import lombok.Value;
+import org.springframework.lang.Nullable;
 
 @Value
+@Builder
 public class ATCdto {
 
-    String atcId;
+    String anatomicalGr;
+    String therapeutSubgr;
+    String pharmacolSubgr;
+    String chemicalSubgr;
+    String chemicalSubst;
 
     public static ATCdto fromATCEntity(ATC atc){
-        return new ATCdto(atc.getATCid());
+        return new ATCdto(
+                atc.getAnatomicalGr(),
+                atc.getTherapeutSubgr(),
+                atc.getPharmacolSubgr(),
+                atc.getChemicalSubgr(),
+                atc.getChemicalSubst()
+        );
+
     }
 }
--- src/main/java/com/example/geriafarm/entities/ATC.java
+++ src/main/java/com/example/geriafarm/entities/ATC.java
@@ -3,10 +3,9 @@ package com.example.geriafarm.entities;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
-import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
@@ -15,35 +14,15 @@ import java.util.UUID;
 public class ATC {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    private String ATCid;
+    private String anatomicalGr;
+    private String therapeutSubgr;
+    private String pharmacolSubgr;
+    @Nullable
+    private String chemicalSubgr;
+    @Nullable
+    private String chemicalSubst;
 
-    public String getAnatomicalGroup(ATC atc) {
-        return atc.getATCid().substring(0, 1);
-    }
 
-    public String getTherapeuticSubgroup(ATC atc) {
-        return atc.getATCid().substring(1, 3);
-    }
-
-    public String getPharmacologicalSubgroup(ATC atc) {
-        if (atc.getATCid().length() > 3) {
-            return atc.getATCid().substring(3, 4);
-        }
-        return "Pharmacological group not given";
-    }
-
-    String getChemicalSubgroup(ATC atc) {
-        if (atc.getATCid().length() > 4) {
-            return atc.getATCid().substring(4, 5);
-        }
-        return "Chemical group not given";
-    }
-
-    String getChemicalSubstance(ATC atc) {
-        if (atc.getATCid().length() > 5) {
-            return atc.getATCid().substring(5);
-        }
-        return "Chemical substance not known";
-    }
 }
